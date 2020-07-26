2981d5d3e74208e17cf6b17496fa719700286193
ken-myers <60851704+ken-myers@users.noreply.github.com>
2020-07-26 16:50:44 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,19 @@
+
 $(document).ready(function(){
 	setMinHeight();
+    doTyping();
 });
 
 $(window).resize(function(){
 	setMinHeight();
 });
 
-$("#contact-form").submit(function(e){
-	onSubmit(e);
+$("#hiddenFrame").on("load", function(){
+	console.log("agaba")
+	$("#contact-form").each(function(){
+       		this.reset()
+	});
+	$("#submitAlert").fadeIn();
 });
 
 function setMinHeight(){
@@ -39,4 +45,56 @@ function onSubmit(e){
     });
 }
 
+function doTyping(){
+
+    function type(string, speed, identifier){
+        r = 0;
+        function factory(c){
+            return function(next) {$(this).append(c);next();};
+        }
+        for (var i = 0; i < string.length; i++) {
+            c = string.charAt(i);
+            // does spaces instantly. looks better for some readson
+            if(c!=' '){
+                $(identifier).delay(speed).queue(factory(c));
+                r+=speed;
+            }else{
+                $(identifier).queue(factory(c));
+            }
+        }
+
+        return r;
+    }
+
+    function backspace(chars, speed, identifier){
+        r=0;
+
+        for (var i = 0; i < chars; i++) {
+            $(identifier).delay(speed).queue(function(next){
+                text = $(this).text();
+                newText = text.substring(0,text.length-1);
+                $(this).text(newText);
+                next();
+            });
+            r+=speed;
+        }
+
+        return r;
+    }
+
+    typingSpeed=75;
+    mistake = "orem ksp";
+    fix  = "ipsum dolor."
+    delay=0;
+    delay+=type(mistake,typingSpeed,"#typyHeader");
+    $("#typyHeader").delay(350);
+    delay+=350;
+    delay+=backspace(3, 115, "#typyHeader");
+    $("#typyHeader").delay(350);
+    delay+=350;
+    delay+=type(fix,typingSpeed,"#typyHeader");
+    $("#typyPara").delay(delay+100);
+    $("#typyPara").fadeIn(2300);
+
+}
 
